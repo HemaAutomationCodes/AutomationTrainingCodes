8d2777f16ea5dacbe30c299dda1312afb281e9ac
Hemalatha <hemalatha25.n@gmail.com>
2016-09-23 21:35:07 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld{
+
+public static String name = "HemaLatha Automation Test Engineer";
+
+public static void main(String... args){
+
+System.out.println("Welcome "+name+" All the Best");
+
+}
+
+
+
+}

5dd92ea26c90df0c3232e4ebc02ee6f1e3cb8efc
Hemalatha <hemalatha25.n@gmail.com>
2016-09-24 08:02:46 +0530

    corrected the compilation error

--- SampleAdd.java
+++ SampleAdd.java
@@ -4,8 +4,8 @@ public static void main(String args[]){
 
 if(args.length == 2){
 
-Integer a = Integer.parse(args[0]);
-Integer b = Integer.parse(args[1]);
+Integer a = Integer.parseInt(args[0]);
+Integer b = Integer.parseInt(args[1]);
 Integer c = a+b;
 System.out.println("Sum of the value is:- "+c);
 
